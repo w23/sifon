f0641e38ce705ac54e5cc5320a0845e937900a9d
Ivan 'w23' Avdeev <me@w23.ru>
2013-11-28 01:44:20 +0700

    cifon/cx: gracefully skip NULL objects

--- cifond/cx_obj.c
+++ cifond/cx_obj.c
@@ -9,11 +9,13 @@ void *cx_create(cx_size_t size, const char *classname, void (*dtor)(void*)) {
 }
 
 void *cx_retain(void *o) {
-  ++((cx_obj_ptr)o)->refcount;
+  if (NULL != o)
+    ++((cx_obj_ptr)o)->refcount;
   return o;
 }
 
 void cx_release(void *o) {
+  if (NULL == o) return;
   cx_obj_ptr this = (cx_obj_ptr)o;
   if (--(this->refcount) == 0) {
     this->dtor(this);
