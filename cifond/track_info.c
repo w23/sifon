080ee1f17a98aa40c43df8e52284eb248e1e2f56
Ivan 'w23' Avdeev <me@w23.ru>
2013-12-01 13:25:59 +0700

    cifond: исправление невалидных данных в track_info

--- cifond/track_info.c
+++ cifond/track_info.c
@@ -10,7 +10,14 @@ static void track_info_dtor(void *track) {
 
 track_info_ptr track_info_create(const char *filename, tags_ptr tags) {
   track_info_ptr this = CX_CREATE(track_info);
+  this->id = 0;
   this->filename = cx_string_create(filename);
+  this->flags = 0;
+  this->duration = 0;
+  this->sample_rate = 0;
+  this->channels = 0;
+  this->bitrate = 0;
+  this->codec_name = NULL;
   this->tags = cx_retain(tags);
   return this;
 }
